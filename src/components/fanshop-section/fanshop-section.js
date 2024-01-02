3c2d144f1e200a60ad91ca6527a3eab9d3a09c67
mizza-cz <mizza.cz666@gmail.com>
2024-01-02 15:28:28 +0100

    add NW

--- src/components/fanshop-section/fanshop-section.js
+++ src/components/fanshop-section/fanshop-section.js
@@ -1,35 +1,26 @@
-// Проверяем наличие элементов на странице
 const productLinks = document.querySelectorAll(".fanshop-section__product");
 const popup = document.getElementById("product-popup");
 const productImage = document.getElementById("product-image");
 const closePopup = document.getElementById("close-popup");
 
-// Проверяем, что все необходимые элементы существуют на странице
 if (productLinks.length > 0 && popup && productImage && closePopup) {
-  // Если элементы существуют, добавляем обработчики событий
-
-  // Обработчик клика на продукт
   productLinks.forEach((productLink) => {
     productLink.addEventListener("click", (event) => {
-      event.preventDefault(); // Предотвращаем переход по ссылке
+      event.preventDefault();
       const imageUrl = productLink.querySelector("img").src;
       productImage.src = imageUrl;
       popup.style.display = "flex";
     });
   });
 
-  // Обработчик клика для закрытия попапа при клике на "Закрыть"
   closePopup.addEventListener("click", () => {
     popup.style.display = "none";
   });
 
-  // Обработчик клика для закрытия попапа при клике на фон или вне изображения продукта
   popup.addEventListener("click", (event) => {
     if (event.target === popup || event.target === productImage) {
       popup.style.display = "none";
     }
   });
 } else {
-  // Если какой-то из элементов отсутствует, выводим сообщение об ошибке
-  console.error("error.");
 }
